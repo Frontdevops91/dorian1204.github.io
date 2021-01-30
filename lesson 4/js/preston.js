b05ec3bd82a338896531cf536da560f664416adf
dorian1204 <lap12005@byui.edu>
2021-01-29 23:49:33 -0700

    footer

--- lesson 4/js/preston.js
+++ lesson 4/js/preston.js
@@ -2,7 +2,8 @@
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hambutton.addEventListener('click', () => {const responsive = 'responsive';
+mainnav.classList.toggle(responsive)}, false);
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
@@ -14,3 +15,17 @@ window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('
 
   classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
   */
+ //current year
+ 
+var n = new Date();
+var y = n.getFullYear();
+document.getElementById("currentyear").innerHTML = y ;
+
+//modified date
+
+var date = document.lastModified;
+document.getElementById("modified").textContent = date;
+
+// var options = { month: "numeric", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric"};
+// var d = document.lastModified().toLocaleDateString("en-US", options);
+// document.getElementById("modified").textContent = d ;
