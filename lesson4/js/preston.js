6ea6a04c33dc202916b55d9d87c876435598accc
dorian1204 <lap12005@byui.edu>
2021-01-31 23:47:53 -0700

    date and time now

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -22,9 +22,16 @@ var y = n.getFullYear();
 document.getElementById("currentyear").innerHTML = y ;
 
 //modified date
+let b = new Date();
+let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-var date = document.lastModified;
-document.getElementById("modified").textContent = date;
+document.getElementById("date").innerHTML = day[b.getDay()] + ", " + 
+b.getDate() + " " + month[b.getMonth()] + " " + b.getFullYear();
+
+function toggleMenu(){
+    document.getElementById("nav-menu").classList.toggle("hide");
+  }
 
 // var options = { month: "numeric", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric"};
 // var d = document.lastModified().toLocaleDateString("en-US", options);
